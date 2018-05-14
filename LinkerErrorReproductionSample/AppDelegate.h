112bf9ecfc071857c0ee4e36a68be6ab1773c1dc
Junichi Tsurukawa <junichi-tsurukawa@cookpad.com>
2018-05-14 11:08:09 +0900

    Initial commit

---
+++ LinkerErrorReproductionSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinkerErrorReproductionSample
+//
+//  Created by junichi-tsurukawa on 2018/05/14.
+//  Copyright © 2018年 junichit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LinkerErrorReproductionSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LinkerErrorReproductionSample
+//
+//  Created by junichi-tsurukawa on 2018/05/14.
+//  Copyright © 2018年 junichit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
